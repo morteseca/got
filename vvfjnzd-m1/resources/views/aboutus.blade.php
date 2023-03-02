14eec3a984f4aa4e268ec176632fc88db2ecca57
morteseca <morteseca@yahoo.com>
2023-03-02 02:56:33 -0800

    detailed

--- vvfjnzd-m1/resources/views/aboutus.blade.php
+++ vvfjnzd-m1/resources/views/aboutus.blade.php
@@ -1,31 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-<section style="display:flex; flex-direction:column; align-items: center">
+    <div class="sliders"
         <h1>Девиз компании</h1>
         <p>Новинки компании</p>
-        @foreach ($a as $b)
-        <span>
-            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-inner">
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+            @foreach ($a as $b)
+                @if ($loop -> first)
                     <div class="carousel-item active">
-                        <img style="width:500px; height:300px;" src="{{ $b->img }}" alt="*">
+                        <img src="{{ $b->img }}" class="d-block w-100" alt="...">
+                        <p class="corusel-item-name">{{ $b->name }}</p>
                     </div>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
-                    data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Предыдущий</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
-                    data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Следующий</span>
-                </button>
+                @else
+                    <div class="carousel-item">
+                        <img src="{{ $b->img }}" class="d-block w-100" alt="...">
+                        <p class="corusel-item-name">{{ $b->name }}</p>
+                    </div>
+                @endif
+
+            @endforeach
             </div>
-        </span>
-        @endforeach
-        <p><button class="submit"><a href="{{ route('home') }}">Вернуться на главную</a></button></p>
-    </section>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+        </div>
+    </div>
 @endsection
-    
--- vvfjnzd-m1/storage/framework/views/734b264e3f4049affc98980de8064ca48e69d860.php
+++ vvfjnzd-m1/storage/framework/views/734b264e3f4049affc98980de8064ca48e69d860.php
@@ -1,32 +1,35 @@
-
-
 <?php $__env->startSection('content'); ?>
-<section style="display:flex; flex-direction:column; align-items: center">
+    <div class="sliders"
         <h1>Девиз компании</h1>
         <p>Новинки компании</p>
-        <?php $__currentLoopData = $a; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $b): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-        <span>
-            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-inner">
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+            <?php $__currentLoopData = $a; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $b): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                <?php if($loop -> first): ?>
                     <div class="carousel-item active">
-                        <img style="width:500px; height:300px;" src="<?php echo e($b->img); ?>" alt="*">
+                        <img src="<?php echo e($b->img); ?>" class="d-block w-100" alt="...">
+                        <p class="corusel-item-name"><?php echo e($b->name); ?></p>
                     </div>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
-                    data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Предыдущий</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
-                    data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Следующий</span>
-                </button>
+                <?php else: ?>
+                    <div class="carousel-item">
+                        <img src="<?php echo e($b->img); ?>" class="d-block w-100" alt="...">
+                        <p class="corusel-item-name"><?php echo e($b->name); ?></p>
+                    </div>
+                <?php endif; ?>
+
+            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
             </div>
-        </span>
-        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-        <p><button class="submit"><a href="<?php echo e(route('home')); ?>">Вернуться на главную</a></button></p>
-    </section>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+        </div>
+    </div>
 <?php $__env->stopSection(); ?>
-    
+
 <?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /srv/users/dbcsbhcc/vvfjnzd-m1/resources/views/aboutus.blade.php ENDPATH**/ ?>
